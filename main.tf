55a9e447ecea85cafbf574772bf68c0d0d013463
Myngahuynh <ngahm277@gmail.com>
2024-10-31 10:25:55 +0700

    update 7

--- main.tf
+++ main.tf
@@ -3,9 +3,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "hello" {
-  ami           = "ami-09dd2e08d601bff67"
+  ami           = "ami-047126e50991d067b"
   instance_type = "t2.micro"
   tags = {
     Name = "HelloWorld"
   }
 }
+
